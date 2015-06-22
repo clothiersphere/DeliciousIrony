434e879260fa908fdb77a578adccb2778f091f58
Niall O'Kane <okanenm@gmail.com>
2015-06-22 14:32:49 -0700

    Creating eventModel

--- server/events/eventModel.js
+++ server/events/eventModel.js
@@ -1,8 +1,18 @@
 var mongoose = require('mongoose');
 
 var EventSchema = new mongoose.Schema({
- text: String,
- votes: Number
+ description: { type: String, required: true },
+ created_at: { type: Date },
+ userId: { type: Number, required: true },
+ location: { type: "Point", required: true }
+});
+
+EventSchema.pre('save', function(next) {
+  now = new Date();
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Event', EventSchema);
