13a27fbf53db5bc7107dcdec0b3e72b43a793a7e
David Hom <dlhom3@gmail.com>
2015-06-23 10:47:46 -0700

    (feat) added db user controller with dummy models

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,10 +1,51 @@
 var User = require('./userModel');
 
 module.exports = {
-/*  somemethod: function(req, res, next){
-    var newUser = new User({
+  signup: function(req, res, next){
+    var dummy = User({ // dummy user
+      fb_id: 'fb_id',
+      username: 'username',
+      first_name: 'first',
+      last_name: 'last',
+      email: 'abc@abc'
+    });
 
-    })
+    User.findOne({fb_id: dummy.fb_id})
+      .exec(function(err, user){
+        if(err) console.log(err);
+        if(user){
+          res.send(200);
+        } else {
+          User.create(dummy, function(err, user){
+            console.log(dummy.fb_id, 'successfully created');
+          });
+        }
+      });
+
     res.json('hello world');
-  }*/
+  },
+
+  login: function(req, res, next){
+    var dummy = User({ // dummy user
+      fb_id: 'fb_id',
+      username: 'username',
+      first_name: 'first',
+      last_name: 'last',
+      email: 'abc@abc'
+    });
+
+    User.findOne({fb_id: dummy.fb_id})
+      .exec(function(err, user){
+        if(err) console.log(err);
+        if(user){
+          res.send(200);
+        } else {
+          User.create(dummy, function(err, user){
+            console.log(dummy.fb_id, 'successfully created');
+          });
+        }
+      });
+
+    res.json('hello world');
+  }
 };
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,6 +2,7 @@ var userCtrl = require('./userController.js');
 
 module.exports = function(app){
   app.route('/')
-    //.get(userCtrl.method)
-    //.post(userCtrl.method);
+    .get(userCtrl.login)
+    .post(userCtrl.signup);
+
 };
