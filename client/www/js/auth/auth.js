f2b9f3126e05d1a72e3d386b3633806354390825
Ashwin ARAVINDAN <Ashwin@Ashwins-MacBook-Pro.local>
2015-06-23 10:12:59 -0700

    Set up openFB authorization

---
+++ client/www/js/auth/auth.js
@@ -0,0 +1,29 @@
+// do not tamper with this code in here, study it, but do not touch
+// this Auth controller is responsible for our client side authentication
+// in our signup/signin forms using the injected Auth service
+angular.module('starter.auth', ['ngOpenFB'])
+
+.controller('AuthController', function ($scope, $ionicModal, $timeout, ngFB) {
+  
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  $scope.fbLogin = function () {
+    ngFB.login({scope: 'email,read_stream,publish_actions'}).then(
+        function (response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        });
+  };
+});
