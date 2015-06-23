f40c453538375abcd20b55ebcf6a47f5c941cfc3
David Hom <dlhom3@gmail.com>
2015-06-23 16:07:35 -0700

    (refactor) refactor db user to use promise Q

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,20 +1,24 @@
 var User = require('./userModel');
+var Q = require('q');
 
 module.exports = {
   login: function(req, res, next){
-    var data = req.body;
 
-    User.findOne({id: data.id})
-      .exec(function(err, user){
-        if(err) console.log(err);
-        if(user){
+    var findOne = Q.nbind(User.findOne, User);
+
+    findOne({id: req.body.id})
+      .then(function (user) {
+        if (user) {
           res.sendStatus(200);
         } else {
-          User.create(data, function(err, user){
-            console.log(user, 'successfully created');
+          var createUser = Q.nbind(User.create, User);
+          return createUser(req.body).then(function(user){
             res.sendStatus(201);
           });
         }
+      })
+      .fail(function (error) {
+        next(error);
       });
   }
 };
