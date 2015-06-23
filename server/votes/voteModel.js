68c4e31ff524ef4167b63847fda95f70dccb7db9
Niall O'Kane <okanenm@gmail.com>
2015-06-23 16:53:14 -0700

    Implementing VoteSchema

--- server/votes/voteModel.js
+++ server/votes/voteModel.js
@@ -1,7 +1,27 @@
 var mongoose = require('mongoose');
 
-var VoteSchema = new mongoose.Schema({
+/*
+The 'type' field refers to an upvote or a downvote. 
+A value of -1 indicates a downvote. 
+A value of 1 indicates an upvote.
+*/
 
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+var VoteSchema = new mongoose.Schema({
+  userId: { type: Number, required: true },
+  eventId: { type: ObjectId, required: true},
+  type: { type: Number, required: true },
+  created_at: { type: Date },
 });
+
+VoteSchema.pre('save', function(next) {
+  var now = new Date();
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
 
 module.exports = mongoose.model('votes', VoteSchema);
