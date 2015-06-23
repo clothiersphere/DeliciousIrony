7d847203521368039c1bb387ee32725cd1e28875
David Hom <dlhom3@gmail.com>
2015-06-23 14:21:02 -0700

    (fix) fixed db user model and controller

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,6 +2,5 @@ var userCtrl = require('./userController.js');
 
 module.exports = function(app){
   app.route('/')
-    .get(userCtrl.login)
-    .post(userCtrl.signup);
+    .post(userCtrl.login);
 };
