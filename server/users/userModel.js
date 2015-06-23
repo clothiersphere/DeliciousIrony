20d7914f74c6c30d8d399cb3f690c294185231b3
David Hom <dlhom3@gmail.com>
2015-06-23 11:22:52 -0700

    (fix) removed username from user db model

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -4,8 +4,7 @@ var UserSchema = new mongoose.Schema({
   fb_id: { type: String, required: true, unique: true },  
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
-  email: { type: String, required: true },
-  username: { type: String, required: true, unique: true }
+  email: { type: String, required: true }
 });
 
 module.exports = mongoose.model('users', UserSchema);
