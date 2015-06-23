f7a857e1d76cebe6850825dcb1a32169124030ff
Niall O'Kane <okanenm@gmail.com>
2015-06-23 13:15:10 -0700

    Added 'loc' field to EventSchema. Indexed the 'loc' field

--- server/events/eventModel.js
+++ server/events/eventModel.js
@@ -3,8 +3,12 @@ var mongoose = require('mongoose');
 var EventSchema = new mongoose.Schema({
   description: { type: String, required: true },
   created_at: { type: Date },
+  loc: { type: { type: String }, coordinates: []},
   userId: { type: Number, required: true }
 });
+
+//This creates an additional index at the loc field. This enables users to find $near event locations 
+EventSchema.index({loc: '2dsphere'});
 
 EventSchema.pre('save', function(next) {
   var now = new Date();
