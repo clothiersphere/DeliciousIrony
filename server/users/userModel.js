42decf7b1a905d083aadce4025fd1d8970dd1604
Niall O'Kane <okanenm@gmail.com>
2015-06-24 11:42:51 -0700

    prepend '_' to UserSchema 'id'

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },  
+  _id: { type: String, required: true, unique: true },  
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true }
