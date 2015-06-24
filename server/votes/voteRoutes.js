d09299785ec2b79bda6197afcde236396ed0b310
Niall O'Kane <okanenm@gmail.com>
2015-06-24 10:50:35 -0700

    Defined GET and POST routes for /api/votes

--- server/votes/voteRoutes.js
+++ server/votes/voteRoutes.js
@@ -2,6 +2,6 @@ var voteCtrl = require('./voteController.js');
 
 module.exports = function(app){
   app.route('/')
-    //.get(voteCtrl.extract)
-    //.post(voteCtrl.add);
+    .get(voteCtrl.extract)
+    .post(voteCtrl.add);
 };
