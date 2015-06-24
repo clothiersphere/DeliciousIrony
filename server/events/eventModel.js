948d1ee325bc37b258f4ce71f3dee8dd8e9567fd
Niall O'Kane <okanenm@gmail.com>
2015-06-24 14:30:14 -0700

    Remove EventSchema.pre('save'), Add 'user' and 'votes' field to EventSchema

--- server/events/eventModel.js
+++ server/events/eventModel.js
@@ -1,21 +1,23 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var EventSchema = new mongoose.Schema({
   description: { type: String, required: true },
   created_at: { type: Date },
   loc: { type: { type: String }, coordinates: []},
-  userId: { type: Number, required: true }
+  /*
+  user is the '_id' of the User Model that created 
+  this Event model
+  */
+  user: { type: String, ref: 'users' },
+  /*
+  votes is an array of Vote Model '_id'
+  */
+  votes: [{ type: Schema.Types.ObjectId, ref: 'votes'}]
 });
+
 
 //This creates an additional index at the loc field. This enables users to find $near event locations 
 EventSchema.index({loc: '2dsphere'});
-
-EventSchema.pre('save', function(next) {
-  var now = new Date();
-  if ( !this.created_at ) {
-    this.created_at = now;
-  }
-  next();
-});
 
 module.exports = mongoose.model('events', EventSchema);
