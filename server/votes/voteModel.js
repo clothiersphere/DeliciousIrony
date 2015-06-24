ae6df51ab37be501b92900651d64fd45908f3a57
Niall O'Kane <okanenm@gmail.com>
2015-06-24 14:23:31 -0700

    userId is now a String. Removed VoteSchema.pre('save')

--- server/votes/voteModel.js
+++ server/votes/voteModel.js
@@ -11,32 +11,11 @@ A value of 1 indicates an upvote.
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 var VoteSchema = new mongoose.Schema({
-  userId: { type: Number, required: true },
+  userId: { type: String, required: true },
   eventId: { type: ObjectId, required: true},
   type: { type: Number, required: true },
   created_at: { type: Date },
   event_created_at: { type: Date }
 });
-
-VoteSchema.pre('save', function(next) {
-  var now = new Date();
-  if ( !this.created_at ) {
-    this.created_at = now;
-  }
-  if ( !this.event_date ) {
-    var findOneEvent = Q.nbind(Event.findOne, Event);
-    findOneEvent.apply(this,{ "_id" : this.eventId })
-    .then(function(event) {
-      this.event_created_at = event.created_at;
-      this.save();
-    }.bind(this))
-    .fail(function(err) {
-      console.log(err);
-      return;
-    });
-  }
-  next();
-});
-
 
 module.exports = mongoose.model('votes', VoteSchema);
