9167359896401cb05ef50411d236b327a5c30f5a
Niall O'Kane <okanenm@gmail.com>
2015-06-25 09:49:18 -0700

    (cleanup) jshint

--- server/votes/voteController.js
+++ server/votes/voteController.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Q = require('q');
 
 module.exports = {
-  extract: function(req, res, next) {
+  extract: function (req, res, next) {
     /*
       extract() is used when the Server receives a GET request at /api/votes
       Finding all Vote Documents that have a 'event_created_at' value
@@ -15,19 +15,19 @@ module.exports = {
     lastTime.setHours(lastTime.getHours() - 12);
 
     var findVotes = Q.nbind(Vote.find, Vote);
-    findVotes({ "event_created_at": { $gt : lastTime }})
-    .then(function(votes) {
+    findVotes({ 'event_created_at': { $gt : lastTime }})
+    .then(function (votes) {
       console.log('Votes Successfully Extracted');
       res.json({ votes: votes });
     })
-    .fail(function(error) {
-      console.log("Vote Extraction failed");
+    .fail(function (error) {
+      console.log('Vote Extraction Failed');
       next(error);
     });
 
-  }, 
+  },
 
-  add: function(req, res, next) {
+  add: function (req, res, next) {
     /*
       add() is used when the Server receives a POST request at /api/votes
 
@@ -40,33 +40,33 @@ module.exports = {
         userId = req.body.userId,
         type = req.body.type;
 
-    var createVote = Q.nbind(Vote.create, Vote); 
+    var createVote = Q.nbind(Vote.create, Vote);
     createVote({
         eventId: eventId,
         userId: userId,
         type: type
-    })
-    .then(function(vote) {
-      Event.findOne({ "_id" : vote.eventId })
-      .exec(function(err,event) {
-        var now = new Date();
-        if ( !vote.created_at ) {
-          vote.created_at = now;
-        }
-        vote.event_created_at = event.created_at;
-        vote.save(function(err, voteSaved) {
-          console.log("Created Vote");
-          event.votes.push(voteSaved._id);
-          event.save(function(err, eventSaved) {
-            console.log("Push VoteID into the event");
-            res.sendStatus(201);
-          })
+      })
+    .then(function (vote) {
+        Event.findOne({ '_id' : vote.eventId })
+      .exec(function (err, event) {
+          var now = new Date();
+          if (!vote.created_at) {
+            vote.created_at = now;
+          }
+          vote.event_created_at = event.created_at;
+          vote.save(function (err, voteSaved) {
+            console.log('Created Vote');
+            event.votes.push(voteSaved._id);
+            event.save(function (err, eventSaved) {
+              console.log('Push VoteID into the event');
+              res.sendStatus(201);
+            });
+          });
         });
+      })
+    .fail(function (err) {
+        console.log('Error on Vote Creation', err);
+        return;
       });
-    })
-    .fail(function(err) {
-      console.log("error on vote creation", err);
-      return;
-    });
   }
 };
