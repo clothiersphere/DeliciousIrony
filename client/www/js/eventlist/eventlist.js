b5ba3eaae00fe9ecdc1b1123e5b3832908d40213
Ashwin Aravindan <Ashwin@Ashwins-MBP.att.net>
2015-06-25 01:11:05 -0700

    Sorted events by location and time. Added loading icon. Added create event button.

--- client/www/js/eventlist/eventlist.js
+++ client/www/js/eventlist/eventlist.js
@@ -1,14 +1,16 @@
 angular.module('starter.eventlist', [])
 
-.controller('EventListController', function ($scope, Events, Token) {
+.controller('EventListController', function ($scope, $q, $location, Events, LocationService, Token) {
 
   $scope.data = {};
   $scope.vote = {};
+  $scope.ready = false;
 
   $scope.getEvents = function(){
     Events.getEvents().then(function(events){
       console.log("Events in eventlist: " + JSON.stringify(events.events));
       $scope.data.events = events.events;
+      var prom = [];
       $scope.data.events.forEach(function(event) {
         event.upvoteCount = 0;
         event.downvoteCount = 0;
@@ -22,8 +24,16 @@ angular.module('starter.eventlist', [])
           if (vote.userId === Token.get('userId')) {
             event.alreadyVoted = true;
           }
-        })
-      })
+        });
+        prom.push(LocationService.getlongLat().then(function(coordinates) {
+          console.log("Current coordinates: " + coordinates);
+          console.log("Event coordinates: " + event.loc.coordinates);
+          event.distance = $scope._calcDistance(coordinates[1], coordinates[0], event.loc.coordinates[1], event.loc.coordinates[0]);
+        }));
+      });
+      $q.all(prom).then(function() {
+        $scope.ready = true;
+      });
     });
   };
 
@@ -55,7 +65,30 @@ angular.module('starter.eventlist', [])
       });        
   };
 
+  $scope._calcDistance = function(lat1, lon1, lat2, lon2) {
+    var R = 6371000; // metres
+    var φ1 = (lat1) * Math.PI / 180;
+    var φ2 = (lat2) * Math.PI / 180;
+    var Δφ = (lat2-lat1) * Math.PI / 180;
+    var Δλ = (lon2-lon1) * Math.PI / 180;
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    var d = R * c * 0.000621371;
+    var miles = +d.toFixed(2);
+
+    return miles;
+  };
+
+  $scope.createEventModal = function() {
+    $location.path('/createevent');
+  }
+
   $scope.init = function() {
+    $scope.ready = false;
     $scope.getEvents();
   };
 
