2a4fa89d5d994fc28c04918d662c4a7142a601e8
Spacemixup <xchrislee@gmail.com>
2015-06-25 14:25:35 -0700

    (cleanup) - fixed eventlist.js lint v2

--- client/www/js/eventlist/eventlist.js
+++ client/www/js/eventlist/eventlist.js
@@ -94,19 +94,19 @@ angular.module('starter.eventlist', ['angularMoment'])
 
   $scope.signout = function () {
     Token.signout();
-  }
+  };
 
   $scope._calcDistance = function (lat1, lon1, lat2, lon2) {
     var R = 6371000; // metres
     var φ1 = (lat1) * Math.PI / 180;
     var φ2 = (lat2) * Math.PI / 180;
-    var Δφ = (lat2-lat1) * Math.PI / 180;
-    var Δλ = (lon2-lon1) * Math.PI / 180;
+    var Δφ = (lat2 - lat1) * Math.PI / 180;
+    var Δλ = (lon2 - lon1) * Math.PI / 180;
 
-    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
             Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     var d = R * c * 0.000621371;
     var miles = +d.toFixed(1);
